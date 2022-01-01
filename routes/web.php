340312dba7a72b8f25afa41d0bf05f3e7e295913
Kathleen Febiola <56445175+ReiKath26@users.noreply.github.com>
2022-01-01 20:04:02 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ShippingAddressController;
+use App\Http\Controllers\ShippingController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +26,32 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/login', function(){
+    return view('login');
+});
+Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::get('/about-us', function(){
     return view('about');
 });
 
-Route::get('/all-books', function(){
-    return view('allBooks');
-});
+Route::get('/all-books', [BookController::class, 'show']);
+Route::get('/books/{id}', [BookController::class, 'detail']);
 
 Route::get('/contact-us', function(){
     return view('conttact');
+});
+
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::resource('cart', CartController::class);
+    Route::patch('/empty/{id}', [CartController::class, 'empty']);
+    Route::resource('cartDetail', DetailController::class);
+    Route::resource('shippingaddress', ShippingAddressController::class);
+    Route::resource('shipping', ShippingController::class);
+    Route::resource('payment', PaymentController::class);
+    Route::post('/order/billing', [TransactionController::class, 'store']);
+    Route::get('/checkout', [CartController::class, 'checkout']);
+    Route::get('/payment-proof', [UploadController::class, 'upload']);
+    Route::post('/upload/process', [UploadController::class. 'process']);
 });
