3f8462ccba5a898e5ba9fabd45af828ba4503c02
Ryani Yuan Jie Lee <92898827+RyaniLee@users.noreply.github.com>
2022-01-12 22:34:48 +0700

    Add new functions for allBooks Page
    add new functions for sorting according button chosen

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,8 +10,41 @@ class BookController extends Controller
     function show()
     {
         $Books = book::all();
-        return view('allBooks', ['books'=>$Books]);
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$Books, 'categories'=>$Categories]);
     }
+
+    function showBookByCategory($name){
+        $selectBook = book::select('books.*')
+        ->join('categories', 'categories.id', '=', 'books.categories_id')->where('categories.name', $name)->get();
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$selectBook, 'categories'=>$Categories]);
+    }
+
+    function showBookAvaible(){
+        $selectBook = book::select('books.*')->where('books.stock', '>', 0)->get();
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$selectBook, 'categories'=>$Categories]);
+    }
+
+    function showBookNewest(){
+        $selectBook = book::orderByDesc('id')->get();
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$selectBook, 'categories'=>$Categories]);
+    }
+
+    function showBookLowestPrice(){
+        $selectBook = book::orderBy('price', 'asc')->get();
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$selectBook, 'categories'=>$Categories]);
+    }
+
+    function showBookTopSelling(){
+        $selectBook = book::orderBy('sold', 'desc')->get();
+        $Categories = Categories::all();
+        return view('allBooks', ['books'=>$selectBook, 'categories'=>$Categories]);
+    }
+
 
     function bookShow()
     {
